4564e8653dced2f3357e4a22e2d6fbbb0cc4a717
RudyZhang011022 <98185299+RudyZhang011022@users.noreply.github.com>
2024-05-10 15:53:48 -0400

    Update Registration.sh

--- Registration.sh
+++ Registration.sh
@@ -1,6 +1,7 @@
 # change all /Users/rudyzhang to the directory where your ANTs code is (where the ANTs folder is)
 export PATH=/Users/rudyzhang/ANTs/install/bin:$PATH
 ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=8
+# change /ANTS_test to the directory where you intend to put input file output file folders, as well as this script, at
 cd /Users/rudyzhang/ANTs/install/bin/ANTS_test
 
 # T1w synthesis, assuming all input files are located in ./data and all output files goes to ./output
